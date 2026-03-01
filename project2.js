fc471b8b56babb8eb1c8d109c5a764babe929c57
GAGAN <gagankarande@gmail.com>
2026-03-01 14:19:29 +0530

    add new feature

--- project2.js
+++ project2.js
@@ -1,1 +1,11 @@
 //add new feature
+// Pull Request:-
+// It lets you tell others about changes you've pushed to a branch in a repository on GitHub.
+
+// git pull origin main
+// used to fetch and download content from a remote repo and immediately update the local
+// repo to match that content.
+
+// Merge Conflicts:-
+// An event that takes place when Git is unable to automatically resolve diffferences in code
+// between two commits.
